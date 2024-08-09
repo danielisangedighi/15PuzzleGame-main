1ba55a220aedab33d2cb60474e6937d4bdbf82c0
Daniel Isangedighi <danielisangedighi@outlook.com>
2024-08-09 20:36:47 +0100

    view updates

--- src/main/java/com/example/puzzle/controller/GameController.java
+++ src/main/java/com/example/puzzle/controller/GameController.java
@@ -2,12 +2,19 @@ package com.example.puzzle.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GameController {
 
-    @GetMapping("/puzzlePage")
-    public String showPuzzlePage() {
-        return "puzzlePageView";
+    @GetMapping("/index")
+    public String showHomePage() {
+        return "index";
     }
+
+    @PostMapping("/puzzlePage")
+    public String showPuzzlePage() {
+        return "puzzlePage";
+    }
+    
 }
